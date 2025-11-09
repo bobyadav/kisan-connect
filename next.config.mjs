42738d6af8f4d6b1fc2bb675c831c95d9f2de489
bobyadav <109846702+bobyadav@users.noreply.github.com>
2025-11-09 08:40:23 +0545

    Add files via upload

--- next.config.mjs
+++ next.config.mjs
@@ -1,28 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  eslint: {
-    ignoreDuringBuilds: true,
-  },
   typescript: {
     ignoreBuildErrors: true,
   },
   images: {
-    remotePatterns: [
-      {
-        protocol: 'https',
-        hostname: '**',
-      },
-    ],
     unoptimized: true,
-  },
-  redirects: async () => {
-    return [
-      {
-        source: '/home',
-        destination: '/',
-        permanent: true,
-      },
-    ]
   },
 }
 
